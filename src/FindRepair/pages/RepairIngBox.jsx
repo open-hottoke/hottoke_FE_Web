da93f8d5a07c19da5e41424f298381fc5e83c062
황선우 <hwangsunwoo@hwangseon-uui-MacBookPro.local>
2024-12-17 23:17:21 +0900

    fix: 주소 수정

--- src/FindRepair/pages/RepairIngBox.jsx
+++ src/FindRepair/pages/RepairIngBox.jsx
@@ -61,29 +61,35 @@ const ToDetailButton = styled.div`
 `;
 
 const RepairIngBox = () => {
-  const result01 = ['', '', ''];
+  // 데이터 배열 구성
+  const repairData = [
+    { address: '서울특별시 동작구 상도로 369', type: '문/창문 수리', date: '2024. 11. 20' },
+    { address: '서울특별시 강남구 삼성로 123', type: '배관 누수 수리', date: '2024. 11. 18' },
+    { address: '서울특별시 서초구 반포대로 456', type: '전기 수리', date: '2024. 11. 15' },
+  ];
+
   return (
     <GreyBox>
       <div className="h1" style={{ textAlign: 'left', margin: '0px 0px 10px 10px' }}>
         진행중인 수리
       </div>
       <RowWrapper style={{ gap: '15px', justifyContent: 'left' }}>
-        {result01.map((item, index) => (
+        {repairData.map((item, index) => (
           <div key={index}>
             <WhiteBox>
               <ColumnWrapper>
                 <div className="h2" style={{ textAlign: 'left' }}>
-                  서울특별시 동작구 상도로 369
+                  {item.address}
                 </div>
                 <div className="body2" style={{ color: '#3C66FF', textAlign: 'left' }}>
-                  문/창문 수리
+                  {item.type}
                 </div>
                 <RowWrapper style={{ justifyContent: 'left', gap: '12px' }}>
                   <div className="body2" style={{ color: '#1F1F1F' }}>
                     요청날짜
                   </div>
                   <div className="body1" style={{ color: '#1F1F1F' }}>
-                    2024. 11. 20
+                    {item.date}
                   </div>
                 </RowWrapper>
               </ColumnWrapper>
